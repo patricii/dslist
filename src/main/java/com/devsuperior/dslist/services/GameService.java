7650dc78c1a9dc99f30f26932e8dd916a2dffb8e
patricii <thqv74@gmail.com>
2023-05-10 11:22:09 -0300

    Projections listId , games

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 import jakarta.transaction.Transactional;
@@ -26,6 +27,11 @@ public class GameService {
 	@Transactional
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();	
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	@Transactional
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
